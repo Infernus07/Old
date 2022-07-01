b8ef1a713f614ef6281a995bd2263c94a14c081a
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-01 23:58:01 +0530

    Update emoji.py

--- plasma/emoji.py
+++ plasma/emoji.py
@@ -2,40 +2,24 @@ from __future__ import annotations
 
 __all__ = (
     "Emoji",
-    "Emote"
+    "emoji"
 )
 
 
 class Emoji:
-    def __init__(self, id: int, *, animated: bool = False):
-        if not isinstance(id, int):
-            raise TypeError(f"Expected int parameter, received {id.__class__.__name__} instead.")
+    def _get_emoji(id: int, *, animated: bool = False) -> Emoji:
+        return f"<a:_:{id}>" if animated else f"<:_:{id}>"
 
-        if not isinstance(animated, bool):
-            raise TypeError(f"Expected bool parameter, received {animated.__class__.__name__} instead.")
 
-        self.id = id
-        self.animated = animated
-
-    def __repr__(self) -> str:
-        return f"<Emoji id={self.id}>"
-
-    def __str__(self) -> str:
-        return f"<a:_:{self.id}>" if self.animated else f"<:_:{self.id}>"
-
-    def __int__(self) -> int:
-        return self.id
-
-    def __hash__(self) -> int:
-        return hash(self.id)
+    # Custom
 
     @classmethod
     def check(self) -> Emoji:
-        return str(Emoji(990273123444207636))
+        return self._get_emoji(990273123444207636)
 
     @classmethod
     def cross(self) -> Emoji:
-        return str(Emoji(990273148379332678))
+        return self._get_emoji(990273148379332678)
 
 
-Emote = Emoji
+emoji = Emoji
