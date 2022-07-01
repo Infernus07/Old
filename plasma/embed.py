0d40917389690cf878f451599914f5b9534ab7bf
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-02 00:06:52 +0530

    Update embed.py

--- plasma/embed.py
+++ plasma/embed.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import nextcord
 from typing import Literal
 
@@ -10,25 +10,26 @@ __all__ = (
 )
 
 Mode = Literal["G", "R"]
+EmptyEmbed = nextcord.Embed.Empty
 
 
 class Embed(nextcord.Embed):
     def __init__(
         self,
-        description: str = nextcord.Embed.Empty,
+        description: str = EmptyEmbed,
         *,
         color: int = Color.blue(),
-        title: str = nextcord.Embed.Empty,
-        timestamp: datetime.datetime | bool = None,
+        title: str = EmptyEmbed,
+        timestamp: datetime | bool = None,
         mode: Mode = None
     ):
         self.description = description
         self.mode = mode
-
+        
         if mode is not None:
             color, description = self._get_mode_values()
 
-        timestamp = timestamp if timestamp is not True else datetime.datetime.utcnow()
+        timestamp = timestamp if timestamp is not True else datetime.utcnow()
         super().__init__(color=color, title=title, description=description, timestamp=timestamp)
 
     def _get_mode_values(self) -> tuple[Color, str]:
