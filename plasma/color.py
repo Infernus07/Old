0a9bf1b67d6ce39157d31b3d172f459b21cbf6cb
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-01 23:57:35 +0530

    Update color.py

--- plasma/color.py
+++ plasma/color.py
@@ -4,47 +4,17 @@ import random
 
 __all__ = (
     "Color",
-    "Colour"
+    "color"
 )
 
 
 class Color:
-    def __init__(self, value: int):
-        if not isinstance(value, int):
-            raise TypeError(f"Expected int parameter, received {value.__class__.__name__} instead.")
-
-        self.value = value
-
-    def _get_byte(self, byte: int) -> int:
-        return (self.value >> (8 * byte)) & 0xFF
-
-    def __repr__(self) -> str:
-        return f"<Colour value={self.value}>"
-
-    def __str__(self) -> str:
-        return f"#{self.value:0>6x}"
-
-    def __int__(self) -> int:
-        return self.value
-
-    def __hash__(self) -> int:
-        return hash(self.value)
-
-    @property
-    def r(self) -> int:
-        return self._get_byte(2)
-
-    @property
-    def g(self) -> int:
-        return self._get_byte(1)
-
-    @property
-    def b(self) -> int:
-        return self._get_byte(0)
+    def _get_color(value: int) -> Color:
+        return value
 
     @classmethod
     def from_rgb(self, r: int, g: int, b: int) -> Color:
-        return int(Color((r << 16) + (g << 8) + b))
+        return self._get_color((r << 16) + (g << 8) + b)
 
     @classmethod
     def from_hsv(self, h: float, s: float, v: float) -> Color:
@@ -53,111 +23,117 @@ class Color:
 
     @classmethod
     def default(self) -> Color:
-        return int(Color(0))
+        return self._get_color(0)
 
     @classmethod
     def random(self) -> Color:
         return self.from_hsv(random.random(), 1, 1)
 
+
+    # Default
+
     @classmethod
     def blue(self) -> Color:
-        return int(Color(0x3498DB))
+        return self._get_color()
 
     @classmethod
     def blurple(self) -> Color:
-        return int(Color(0x5865F2))
+        return self._get_color()
 
     @classmethod
     def dark_theme(self) -> Color:
-        return int(Color(0x36393F))
+        return self._get_color()
 
     @classmethod
     def gold(self) -> Color:
-        return int(Color(0xF1C40F))
+        return self._get_color()
 
     @classmethod
     def green(self) -> Color:
-        return int(Color(0x2ECC71))
+        return self._get_color()
 
     @classmethod
     def greyple(self) -> Color:
-        return int(Color(0x99AAB5))
+        return self._get_color()
 
     @classmethod
     def magenta(self) -> Color:
-        return int(Color(0xE91E63))
+        return self._get_color()
 
     @classmethod
     def og_blurple(self) -> Color:
-        return int(Color(0x7289DA))
+        return self._get_color()
 
     @classmethod
     def orange(self) -> Color:
-        return int(Color(0xE67E22))
+        return self._get_color()
 
     @classmethod
     def purple(self) -> Color:
-        return int(Color(0x9B59B6))
+        return self._get_color()
 
     @classmethod
     def red(self) -> Color:
-        return int(Color(0xE74C3C))
+        return self._get_color()
 
     @classmethod
     def teal(self) -> Color:
-        return int(Color(0x1ABC9C))
+        return self._get_color()
 
     @classmethod
     def yellow(self) -> Color:
-        return int(Color(0xFEE75C))
+        return self._get_color()
+
+
+    # Custom
 
     @classmethod
     def amaranth(self) -> Color:
-        return int(Color(0xe52b50))
+        return self._get_color()
 
     @classmethod
     def blush(self) -> Color:
-        return int(Color(0xe69296))
+        return self._get_color()
 
     @classmethod
     def fuchsia(self) -> Color:
-        return int(Color(0xff00ff))
+        return self._get_color()
 
     @classmethod
     def ice(self) -> Color:
-        return int(Color(0xbddeec))
+        return self._get_color()
 
     @classmethod
     def indigo(self) -> Color:
-        return int(Color(0x726eff))
+        return self._get_color()
 
     @classmethod
     def lilac(self) -> Color:
-        return int(Color(0xc8a2c8))
+        return self._get_color()
 
     @classmethod
     def lime(self) -> Color:
-        return int(Color(0xb7ff00))
+        return self._get_color()
 
     @classmethod
     def midnight(self) -> Color:
-        return int(Color(0x007bff))
+        return self._get_color()
 
     @classmethod
     def obsidian(self) -> Color:
-        return int(Color(0x010101))
+        return self._get_color()
 
     @classmethod
     def sky(self) -> Color:
-        return int(Color(0x00c8ff))
+        return self._get_color()
 
     @classmethod
     def snow(self) -> Color:
-        return int(Color(0xa4cce4))
+        return self._get_color()
 
     @classmethod
     def tangerine(self) -> Color:
-        return int(Color(0xef8e38))
+        return self._get_color()
 
 
-Colour = Color
+color = Color
