99679cd232f9be30b9176959dbf5fa8e4abe22da
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-02 19:46:52 +0530

    Update mongo.py

--- plasma/mongo.py
+++ plasma/mongo.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 
+from .errors import PokemonNotFound
+
 __all__ = (
     "Mongo",
     "mongo"
@@ -7,9 +9,17 @@ __all__ = (
 
 
 class Mongo:
-    @property
-    def db(self):
-        return MongoClient("mongodb+srv://Infernus:PgtrAv4wGteOfe2s@plasma.f6kiu.mongodb.net/test")["plasma"]
+    db = MongoClient("mongodb+srv://Infernus:PgtrAv4wGteOfe2s@plasma.f6kiu.mongodb.net/test")["plasma"]
+
+    @classmethod
+    def search_pokemon(self, name: str, *, raise_error: bool = True) -> dict | None:
+        pokemon = self.db["pokemon"].find_one({"names": name.strip().capitalize()})
+        if pokemon is None:
+            if raise_error:
+                raise PokemonNotFound(name)
+            return None
+
+        return pokemon
 
 
 mongo = Mongo
