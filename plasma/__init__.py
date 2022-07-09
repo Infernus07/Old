bec185b01f32753305813df4673f8f5535c33a53
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 11:42:09 +0530

    Update __init__.py

--- plasma/__init__.py
+++ plasma/__init__.py
@@ -1,9 +1,2 @@
 from .errors import *
 from .views import *
-
-
-async def get_confirmation(ctx, message):
-    view = Confirmation(ctx)
-    view.message = await ctx.channel.send(message, view=view)
-    await view.wait()
-    return view.result
