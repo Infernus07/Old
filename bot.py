be47034b2602fc2d69a1969b157027cf2b032c9e
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 14:07:32 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+import plasma
+
+import nextcord
+from nextcord.ext import commands
+
+
+class ClusterBot(commands.AutoShardedBot):
+    def __init__(self):
+        super().__init__(
+            commands.when_mentioned_or("?", ">", "-"),
+            activity=nextcord.Activity(type=nextcord.ActivityType.watching, name="Plasma"),
+            allowed_mentions=nextcord.AllowedMentions(everyone=False),
+            case_insensitive=True,
+            intents=nextcord.Intents.all(),
+            owner_ids=plasma.OWNERS
+        )
+
+        for i in plasma.COGS:
+            self.load_extension(f"cogs.{i}")
+
+        self.add_check(commands.guild_only())
+        self.run()
+
+    @property
+    def mongo(self):
+        return self.get_cog("Mongo")
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+
+
+if __name__ == "__main__":
+    ClusterBot()
