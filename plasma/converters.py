a2dc45e6672355199eb9f700a6b04d09119ad7be
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 20:55:04 +0530

    Update converters.py

--- plasma/converters.py
+++ plasma/converters.py
@@ -1,7 +1,12 @@
+from datetime import timedelta
+from durations_nlp import Duration
+
+import nextcord
 from nextcord.ext import commands
 
 __all__ = (
     "Bot",
+    "TimeDelta"
 )
 
 BOTS = {
@@ -21,3 +26,16 @@ class Bot(commands.Converter):
             return bot
 
         raise commands.BadArgument(f"Could not find a bot matching `{arg}`.")
+
+
+class TimeDelta(commands.Converter):
+    async def convert(self, ctx, arg):
+        try:
+            duration = Duration(arg).to_seconds()
+        except:
+            raise commands.BadArgument(f"{arg} is not a valid duration.")
+
+        if duration > 60 * 60 * 24 * 28:
+            raise commands.BadArgument("Cannot mute for more than 28 days.")
+
+        return nextcord.utils.utcnow() + timedelta(seconds=duration)
