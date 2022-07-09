9df248236d39b8716d4382a13e332f15973704ec
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 18:12:43 +0530

    Update constants.py

--- plasma/constants.py
+++ plasma/constants.py
@@ -11,3 +11,26 @@ COGS = (
 OWNERS = (
     961592504585695233, # Infernus
 )
+
+BANNED_WORDS = (
+    "$uck",
+    "asshole",
+    "b!tch",
+    "b1tch",
+    "bitch",
+    "blowjob",
+    "c0ck",
+    "cnut",
+    "cock",
+    "d1ck",
+    "dick",
+    "fuck",
+    "fuk",
+    "hoe",
+    "nigg",
+    "penis",
+    "pussy",
+    "sex",
+    "s3xy",
+    "vagina"
+)
