387b16bf34b482856611ee875b35541c3fe75d37
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 12:19:20 +0530

    Create mongo.py

---
+++ cogs/mongo.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+from nextcord.ext import commands
+
+
+class Mongo(commands.Cog):
+    """For database operations."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = MongoClient()["plasma"]
+
+def setup(bot):
+    bot.add_cog(Mongo(bot))
