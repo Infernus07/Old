bdd1f4c499e904db6cc9075eb90dfec557d4afe3
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 14:16:05 +0530

    Update autopost.py

--- cogs/autopost.py
+++ cogs/autopost.py
@@ -44,7 +44,11 @@ class AutoPost(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.guild.id == 994266247577485473 and message.author != self.bot.user:
+        if (
+            message.guild is not None and
+            message.guild.id == 994266247577485473 and
+            message.author != self.bot.user
+        ):
             self.updated[message.channel.id] = True
 
     @tasks.loop(minutes=30)
