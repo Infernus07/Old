bcf0e700f105665eab45362dba84e228bcd5f983
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 14:08:18 +0530

    Update constants.py

--- plasma/constants.py
+++ plasma/constants.py
@@ -1,1 +1,13 @@
-COGS = ()
+COGS = (
+    "autopost",
+    "invite_tracker",
+    "mongo",
+    "names",
+    "react",
+    "reaction_roles",
+    "spam"
+)
+
+OWNERS = (
+    961592504585695233, # Infernus
+)
