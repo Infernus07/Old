8c49b1302c53282128eca96ed3f955d225033749
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 11:18:20 +0530

    Update invite_tracker.py

--- cogs/invite_tracker.py
+++ cogs/invite_tracker.py
@@ -12,7 +12,7 @@ class InviteTracker(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         if member.guild.id == 994266247577485473:
-            await self.bot.get_channel(LOG_CHANNEL).send(f"{member} just left the server.")
+            await self.bot.get_channel(LOG_CHANNEL).send(f"{member.mention} just left the server.")
 
 def setup(bot):
     bot.add_cog(InviteTracker(bot))
