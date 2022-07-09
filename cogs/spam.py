44c32674d788e1159d0965a723b297a43363febb
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 12:40:56 +0530

    Create spam.py

---
+++ cogs/spam.py
@@ -0,0 +1,39 @@
+import itertools
+import random
+import requests
+
+from nextcord.ext import commands, tasks
+
+
+SPAMMERS = (
+    "OTIxNDAwNDQ1MjMzMTQzODIw.Yb70XQ._qJ8dVFvLV6pWLYWg1gvZwYhNV0", # Jiya
+    "OTIxMDI2NDAyMjMxNDEwNjk4.YbtajA.Iy48q-yVbLn8s2z1224ZNBYLyNo", # May
+    "OTIxNjEyMjY2ODIxMDE3NjMw.Yb71MA.uQU0WFl3e4iqgyareidLTivIRDM", # Serena
+    "ODg5MDg0MjA2Njc0NzQ3Mzky.YdK5dw.aBvdLbIpvrdyasCMi3xdcNYtF6Y"  # Tyson
+)
+
+class Spam(commands.Cog):
+    """For spam."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.spammers = itertools.cycle(SPAMMERS)
+        self.spam.start()
+
+    @tasks.loop(seconds=0.5)
+    async def spam(self):
+        requests.post(
+            "https://discord.com/api/v9/channels/994922903105720330/messages",
+            {"content": 1},
+            headers={"authorization": next(self.spammers)}
+        )
+
+    @spam.before_loop
+    async def before_spam(self):
+        await self.bot.wait_until_ready()
+
+    async def cog_unload(self):
+        self.spam.cancel()
+
+def setup(bot):
+    bot.add_cog(Spam(bot))
