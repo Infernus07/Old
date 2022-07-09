c4408fe80559273928a8e34bd96cbaa1435d91a5
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-09 12:08:59 +0530

    Update invite_tracker.py

--- cogs/invite_tracker.py
+++ cogs/invite_tracker.py
@@ -1,7 +1,5 @@
 from nextcord.ext import commands
 
-LOG_CHANNEL = 994922728987557918
-
 
 class InviteTracker(commands.Cog):
     """For invite tracker."""
@@ -12,7 +10,8 @@ class InviteTracker(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         if member.guild.id == 994266247577485473:
-            await self.bot.get_channel(LOG_CHANNEL).send(f"{member.mention} just left the server.")
+            channel = self.bot.get_channel(994922728987557918)
+            await channel.send(f"{member.mention} just left the server.")
 
 def setup(bot):
     bot.add_cog(InviteTracker(bot))
