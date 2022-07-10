0a485d6ea84d6ec3b6ea9b0c57ed1dd2f2b74def
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-10 20:50:03 +0530

    Update automod.py

--- cogs/automod.py
+++ cogs/automod.py
@@ -3,6 +3,7 @@ from contextlib import suppress
 from datetime import timedelta
 import plasma
 import re
+import unicodedata
 
 import nextcord
 from nextcord.ext import commands
@@ -14,17 +15,42 @@ class Automod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.cooldown = commands.CooldownMapping.from_cooldown(15, 17, commands.BucketType.member)
-        self.invite_regex = re.compile(r"(?:https?://)?discord(?:app)?\.(?:com/invite|gg)/([a-zA-Z0-9]+)/?", flags=re.I)
+        self.invite_regex = re.compile(plasma.INVITE_REGEX, flags=re.I)
+        self.url_regex = re.compile(plasma.URL_REGEX)
 
-    async def notify(self, message, text):
-        with suppress(nextcord.NotFound):
-            await message.delete()
+    def normalized(self, text):
+        if text is None:
+            return None
+
+        text = unicodedata.normalize("NFKC", text)
+        text = re.sub(self.url_regex, "", text)
+        while len(text) > 0 and text[0] < "0":
+            text = text[1:]
+
+        if len(text) == 0:
+            return None
+        return text[:32]
+
+    async def normalize(self, member):
+        normalized = self.normalized(member.nick) or self.normalized(member.name) or "User"
+        if normalized != member.display_name:
+            await member.edit(nick=normalized)
+
+    async def notify(self, message, text, *, purge=False):
+        if purge:
+            await message.channel.purge(limit=15, check=lambda m: m.author == message.author)
+        else:
+            with suppress(nextcord.NotFound):
+                await message.delete()
         await message.channel.send(f"{message.author.mention} {text}", delete_after=5)
 
     @commands.Cog.listener(name="on_message")
     @commands.Cog.listener(name="on_message_edit")
     async def on_message(self, *args):
         message = args[-1]
+
+        if message.channel.id in (994920672574832742, 995244940340772864):
+            await message.add_reaction(plasma.Emoji.red_tick())
 
         if (
             message.guild is None
@@ -36,7 +62,7 @@ class Automod(commands.Cog):
         ):
             return
 
-        words = message.content.lower().split(" ")
+        words = message.content.lower().split()
         for i in plasma.BANNED_WORDS:
             if any(i in x for x in words):
                 await self.notify(message, "Watch your language!")
@@ -58,8 +84,7 @@ class Automod(commands.Cog):
             bucket = self.cooldown.get_bucket(message)
             if bucket.update_rate_limit():
                 self.cooldown._cache[self.cooldown._bucket_key(message)].reset()
-                await message.channel.purge(limit=15, check=lambda m: m.author == message.author)
-                await message.channel.send(f"{message.author.mention} No spamming!", delete_after=5)
+                await self.notify(message, "No spamming!", purge=True)
                 return
 
     @commands.Cog.listener()
@@ -75,6 +100,21 @@ class Automod(commands.Cog):
             role = member.guild.get_role(994928660157050990)
             await asyncio.sleep(60)
             await member.add_roles(role)
+
+        await self.normalize(member)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        if member.guild.id != 994266247577485473:
+            return
+
+        channel = self.bot.get_channel(994922728987557918)
+        await channel.send(f"{member.mention} just left the server.")
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        if before.nick != after.nick:
+            await self.normalize(after)
 
 def setup(bot):
     bot.add_cog(Automod(bot))
