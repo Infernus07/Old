35563c49bc543e4d1759a2faba069673fe985bb6
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-10 20:41:46 +0530

    Update views.py

--- plasma/views.py
+++ plasma/views.py
@@ -1,18 +1,16 @@
 from contextlib import suppress
+import plasma
 
 import nextcord
 
-__all__ = (
-    "Confirmation",
-    "Pagination"
-)
+__all__ = ("Confirmation", "Pagination", "ColorView")
 
 
 class Confirmation(nextcord.ui.View):
     def __init__(self, ctx):
         super().__init__(timeout=30)
-        self.result = None
         self.ctx = ctx
+        self.result = None
 
     async def interaction_check(self, interaction):
         if interaction.user != self.ctx.author:
@@ -31,22 +29,22 @@ class Confirmation(nextcord.ui.View):
     async def cancel(self, button, interaction):
         await interaction.response.defer()
         await self.message.edit(view=None)
-        await self.ctx.channel.send("Aborted.")
+        await self.ctx.send("Aborted.")
         self.result = False
         self.stop()
 
     async def on_timeout(self):
         with suppress(nextcord.NotFound):
             await self.message.edit(view=None)
-        await self.ctx.channel.send("Time's up. Aborted.")
+            await self.ctx.send("Time's up. Aborted.")
 
 
 class Pagination(nextcord.ui.View):
     def __init__(self, ctx, pages):
         super().__init__(timeout=80)
-        self.paging = 0
         self.ctx = ctx
         self.pages = pages
+        self.paging = 0
 
     async def interaction_check(self, interaction):
         if interaction.user != self.ctx.author:
@@ -71,3 +69,60 @@ class Pagination(nextcord.ui.View):
     async def on_timeout(self):
         with suppress(nextcord.NotFound):
             await self.message.edit(view=None)
+
+
+class ColorMenu(nextcord.ui.Select):
+    def __init__(self):
+        options = [
+            nextcord.SelectOption(label="Amaranth",  emoji=f"{plasma.Emoji.amaranth()}"),
+            nextcord.SelectOption(label="Blush",     emoji=f"{plasma.Emoji.blush()}"),
+            nextcord.SelectOption(label="Fuchsia",   emoji=f"{plasma.Emoji.fuchsia()}"),
+            nextcord.SelectOption(label="Ice",       emoji=f"{plasma.Emoji.ice()}"),
+            nextcord.SelectOption(label="Indigo",    emoji=f"{plasma.Emoji.indigo()}"),
+            nextcord.SelectOption(label="Lilac",     emoji=f"{plasma.Emoji.lilac()}"),
+            nextcord.SelectOption(label="Lime",      emoji=f"{plasma.Emoji.lime()}"),
+            nextcord.SelectOption(label="Midnight",  emoji=f"{plasma.Emoji.midnight()}"),
+            nextcord.SelectOption(label="Obsidian",  emoji=f"{plasma.Emoji.obsidian()}"),
+            nextcord.SelectOption(label="Sky",       emoji=f"{plasma.Emoji.sky()}"),
+            nextcord.SelectOption(label="Snow",      emoji=f"{plasma.Emoji.snow()}"),
+            nextcord.SelectOption(label="Tangerine", emoji=f"{plasma.Emoji.tangerine()}")
+        ]
+        super().__init__(options=options)
+
+    async def callback(self, interaction):
+        for i in plasma.COLOR_ROLES:
+            if any(x.name == i for x in interaction.user.roles):
+                with suppress(nextcord.NotFound):
+                    await interaction.response.defer(ephemeral=True)
+
+                r_role = nextcord.utils.get(interaction.guild.roles, name=i)
+                a_role = nextcord.utils.get(interaction.guild.roles, name=self.values[0])
+                await interaction.user.remove_roles(r_role)
+                await interaction.user.add_roles(a_role)
+
+                embed = nextcord.Embed(
+                    color=nextcord.Color.blue(),
+                    description=f"Removed `{r_role}`\nAdded `{a_role}`"
+                )
+                with suppress(nextcord.NotFound):
+                    await interaction.followup.send(embed=embed, ephemeral=True)
+                return
+
+        with suppress(nextcord.NotFound):
+            await interaction.response.defer(ephemeral=True)
+
+        role = nextcord.utils.get(interaction.guild.roles, name=self.values[0])
+        await interaction.user.add_roles(role)
+
+        embed = nextcord.Embed(
+            color=nextcord.Color.blue(),
+            description=f"{plasma.Emoji.check()} Added `{role}`"
+        )
+        with suppress(nextcord.NotFound):
+            await interaction.followup.send(embed=embed, ephemeral=True)
+
+
+class ColorView(nextcord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.add_item(ColorMenu())
