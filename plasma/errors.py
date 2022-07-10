cafb9b42201e4de33446c5cdd09e34aeeac60670
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-10 20:40:05 +0530

    Update errors.py

--- plasma/errors.py
+++ plasma/errors.py
@@ -1,9 +1,15 @@
-from nextcord.ext import commands
-
-__all__ = (
-    "NotInGuild",
-)
+class DiscordException(Exception):
+    pass
 
 
-class NotInGuild(commands.CheckFailure):
+class CommandError(DiscordException):
+    def __init__(self, message=None, *args):
+        if message is not None:
+            m = message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere")
+            super().__init__(m, *args)
+        else:
+            super().__init__(*args)
+
+
+class NotInGuild(CommandError):
     pass
