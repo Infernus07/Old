2d01bcb2f008b16a4cf71a76073fedda069ddebf
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-10 20:47:31 +0530

    Update reaction_roles.py

--- cogs/reaction_roles.py
+++ cogs/reaction_roles.py
@@ -1,22 +1,9 @@
 from contextlib import suppress
+import plasma
 
 import nextcord
 from nextcord.ext import commands
 
-COLOR_MENU = {
-    "992838901620297799": 994929905974394880, # Amaranth
-    "992838924382785636": 994929940732592148, # Blush
-    "992838948021882920": 994929969509711994, # Fuchsia
-    "992838970109079583": 994930001822621776, # Ice
-    "992838995467845632": 994930024094380052, # Indigo
-    "992839028854493317": 994930211604942918, # Lilac
-    "992839071510569090": 994930238490419250, # Lime
-    "992839096819003512": 994930268274184192, # Midnight
-    "992839124459454525": 994930461572874260, # Obsidian
-    "992839149495267418": 994930496201035857, # Sky
-    "994963175562821666": 994930524160266311, # Snow
-    "992839272824570006": 994930553302298684  # Tangerine
-}
 
 PING_MENU = {
     "📢": 994929600696164372, # Announcement Ping
@@ -24,15 +11,21 @@ PING_MENU = {
     "🎁": 994929518823358645, # Event Ping
     "🎉": 994929467564752967, # Giveaway Ping
     "⚡": 994929560032395346, # Incense Ping
-    "994953711002583131": 994960313596268705 # Pokemon Raid
+    f"{plasma.Emoji.pokemon().id}": 994960313596268705 # Pokemon Raid
 }
 
 ACCESS_MENU = {
-    "994953622330822797": 994929076735311892, # Poketwo Access
-    "994953711002583131": 994929119823405066, # Pokemon Access
-    "994953849452380230": 994929146465615892, # Pokecord Access
-    "994953905203052564": 994929186726740048, # Deriver Access
-    "🤖": 994929186726740048 # Other Bots Access
+    f"{plasma.Emoji.poketwo().id}" : 994929076735311892, # Poketwo Access
+    f"{plasma.Emoji.pokemon().id}" : 994929119823405066, # Pokemon Access
+    f"{plasma.Emoji.pokecord().id}": 994929146465615892, # Pokecord Access
+    f"{plasma.Emoji.deriver().id}" : 994929186726740048, # Deriver Access
+    "🤖": 994929219568156702 # Other Bots Access
+}
+
+RARE_MENU = {
+    f"{plasma.Emoji.pokemon().id}" : 994960527287668826, # Pokemon Rares
+    f"{plasma.Emoji.pokecord().id}": 994960678366494782, # Pokecord Rares
+    f"{plasma.Emoji.deriver().id}" : 994960742103142420  # Deriver Rares
 }
 
 
@@ -42,6 +35,10 @@ class ReactionRoles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def remove_reaction(self, payload, guild, user):
+        message = await guild.get_channel(payload.channel_id).fetch_message(payload.message_id)
+        await message.remove_reaction(payload.emoji, user)
+
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         if payload.user_id != self.bot.user.id:
@@ -50,28 +47,34 @@ class ReactionRoles(commands.Cog):
             emoji = str(payload.emoji.id) if payload.emoji.is_custom_emoji() else payload.emoji.name
             role = None
 
-            if payload.message_id == 995008665260134491:
-                try:
-                    role = guild.get_role(COLOR_MENU[emoji])
-                except:
-                    message = await guild.get_channel(payload.channel_id).fetch_message(payload.message_id)
-                    await message.remove_reaction(payload.emoji, user)
-                    return
-
-            elif payload.message_id == 995028284570095656:
+            if payload.message_id == 995701370785108130:
                 try:
                     role = guild.get_role(PING_MENU[emoji])
                 except:
-                    message = await guild.get_channel(payload.channel_id).fetch_message(payload.message_id)
-                    await message.remove_reaction(payload.emoji, user)
+                    await self.remove_reaction(payload, guild, user)
                     return
 
-            elif payload.message_id == 995031116681592832:
+            elif payload.message_id == 995704076719370240:
                 try:
                     role = guild.get_role(ACCESS_MENU[emoji])
                 except:
-                    message = await guild.get_channel(payload.channel_id).fetch_message(payload.message_id)
-                    await message.remove_reaction(payload.emoji, user)
+                    await self.remove_reaction(payload, guild, user)
+                    return
+
+            elif payload.message_id == 995667586060652604:
+                if (
+                    guild.premium_subscriber_role not in user.roles
+                    and user.id not in plasma.OWNERS
+                    and not any(x.id in [994927712135286828, 994927785351053362, 995608606609252406] for x in user.roles)
+                ):
+                    await self.remove_reaction(payload, guild, user)
+                    with suppress(nextcord.Forbidden):
+                        await user.send("You must be a server booster to get the rares role.")
+                        return
+                try:
+                    role = guild.get_role(RARE_MENU[emoji])
+                except:
+                    await self.remove_reaction(payload, guild, user)
                     return
 
             if role is not None and role not in user.roles:
@@ -87,14 +90,17 @@ class ReactionRoles(commands.Cog):
             emoji = str(payload.emoji.id) if payload.emoji.is_custom_emoji() else payload.emoji.name
             role = None
 
-            if payload.message_id == 995008665260134491:
-                role = guild.get_role(COLOR_MENU[emoji])
+            if payload.message_id == 995701370785108130:
+                with suppress(KeyError):
+                    role = guild.get_role(PING_MENU[emoji])
 
-            elif payload.message_id == 995028284570095656:
-                role = guild.get_role(PING_MENU[emoji])
+            elif payload.message_id == 995704076719370240:
+                with suppress(KeyError):
+                    role = guild.get_role(ACCESS_MENU[emoji])
 
-            elif payload.message_id == 995031116681592832:
-                role = guild.get_role(ACCESS_MENU[emoji])
+            elif payload.message_id == 995667586060652604:
+                with suppress(KeyError):
+                    role = guild.get_role(RARE_MENU[emoji])
 
             if role is not None and role in user.roles:
                 await user.remove_roles(role)
