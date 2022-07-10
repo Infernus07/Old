a7b4d7ae00bf98167f4cd583c0b4b16c5531422c
Gautam756789 <108295553+Infernus07@users.noreply.github.com>
2022-07-10 23:47:50 +0530

    Update mongo.py

--- plasma/mongo.py
+++ plasma/mongo.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 
+from nextcord.ext import commands
+
+from .utils import title
+
 __all__ = ("Mongo", "mongo")
 
 
@@ -10,11 +14,13 @@ class Mongo:
 
         # Collections
 
-        self.pokemon = self.db["pokemon"]
         self.member = self.db["member"]
+        self.pokemon = self.db["pokemon"]
+        self.tag = self.db["tag"]
 
     def species_by_name(self, name):
-        doc = self.pokemon.find_one({"names": name})
+        name = title(name.strip())
+        doc = self.pokemon.find_one({"alias": name})
         if doc is None:
             return None
         return doc
@@ -51,6 +57,33 @@ class Mongo:
             self.register(member)
             
         return self.member.update_one({"_id": member.id}, update)
+
+    def insert_tag(self, *, name, content, owner_id):
+        name = name.strip()
+        content = content.strip()
+        doc = {
+            "_id": name.lower(),
+            "alias": [],
+            "content": content,
+            "owner_id": owner_id,
+            "uses": 0
+        }
+        tag = self.find_tag(name, raise_error=False)
+        if tag is not None:
+            raise commands.BadArgument(f"A tag with the name `{name}` already exists.")
+        
+        return self.tag.insert_one(doc)
+
+    def find_tag(self, name, *, raise_error=True):
+        name = name.strip()
+        doc = self.tag.find_one({"alias": name.lower()})
+        if doc is None and raise_error:
+            raise commands.BadArgument(f"Could not find a tag matching `{name}`.")
+        return doc
+
+    def update_tag(self, name, update):
+        doc = self.find_tag(name)
+        return self.tag.update_one({"_id": doc["_id"]}, update)
 
 
 mongo = Mongo()
